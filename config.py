7002a3040ab9222fb23aaba6d0015ee6f3e89a84
Tumi Steingrímsson <tumi.st@gmail.com>
2013-02-04 23:39:14 +0000

    Running wpmaker.py without source configuration now yield complaints (instead of exceptions)

--- config.py
+++ config.py
@@ -13,6 +13,10 @@ appname = 'wpmaker'
 appauthor = 'viktor'
 config_file_name = '%s.conf' % appname
 
+class ConfigurationError(ValueError):
+    def cmdline_message(self):
+        return "Configuration error: {}".format(self.message)
+
 def get_appdirs_paths():
     appdirs = AppDirs(appname, appauthor)
     dirs = (appdirs.user_data_dir, appdirs.site_data_dir)
