7458505ed8b35b9f7c2ac5cca389d803b27f2b81
Viktor Alex <loktacar@gmail.com>
2013-02-20 00:40:27 +0000

    Bug left by tumist for windows only

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ if os.name == 'nt':
 
 def file_hidden(handle):
     if os.name == 'nt':
-        attribute = win32api.GetFileAttributes(p)
+        attribute = win32api.GetFileAttributes(handle)
         return attribute & (win32con.FILE_ATTRIBUTE_HIDDEN | win32con.FILE_ATTRIBUTE_SYSTEM)
     else:
         return os.path.basename(handle).startswith(".")
